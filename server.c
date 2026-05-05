deb8fda44b121de3e8485173531fc24f28ea5a80
JohnnyCannelloni <yoni.edelstein@gmail.com>
2026-05-05 20:43:34 +0300

    added handling for a single client

--- server.c
+++ server.c
@@ -3,15 +3,13 @@
 #include <unistd.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
 
 #define PORT 8080
 
 int main(void) {
     int server_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_fd < 0) {
-        perror("socket");
-        exit(1);
-    }
+    if (server_fd < 0) { perror("socket"); exit(1); }
 
     struct sockaddr_in addr;
     addr.sin_family = AF_INET;
@@ -23,14 +21,20 @@ int main(void) {
         exit(1);
     }
 
-    if (listen(server_fd, 16) < 0) {
-        perror("listen");
-        exit(1);
-    }
+    if (listen(server_fd, 16) < 0) { perror("listen"); exit(1); } 
 
     printf("Listening on port %d. Press Ctrl-C to quit.\n", PORT);
 
-    pause();
+    struct sockaddr_in client_addr;
+    socklen_t client_len = sizeof(client_addr);
+
+    int client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &client_len);
+    if (client_fd < 0) { perror("accept"); exit(1); }
+
+    printf("Got a connection from %s:%d\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+
+    close(client_fd);
+    close(server_fd);
 
     return 0;
 } 
