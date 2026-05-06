7dc0af7b585b350cee9f5f6240075b2579eab4d6
JohnnyCannelloni <yoni.edelstein@gmail.com>
2026-05-06 16:17:10 +0300

    added looping mechanism when accepting new clients

--- server.c
+++ server.c
@@ -25,15 +25,18 @@ int main(void) {
 
     printf("Listening on port %d. Press Ctrl-C to quit.\n", PORT);
 
-    struct sockaddr_in client_addr;
-    socklen_t client_len = sizeof(client_addr);
+    for (;;) {
+        struct sockaddr_in client_addr;
+        socklen_t client_len = sizeof(client_addr);
 
-    int client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &client_len);
-    if (client_fd < 0) { perror("accept"); exit(1); }
+        int client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &client_len);
+        if (client_fd < 0) { perror("accept"); continue; }
 
-    printf("Got a connection from %s:%d\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+        printf("Got a connection from %s:%d\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
 
-    close(client_fd);
+        close(client_fd);
+    }
+
     close(server_fd);
 
     return 0;
